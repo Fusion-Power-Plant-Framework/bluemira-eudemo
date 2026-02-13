037fb500b14dafb8d183450f44b3489d91536672
Hudson Baker <hudson.baker@ukaea.uk>
2026-02-13 09:18:06 +0000

    create repo with cookiecutter

---
+++ bluemira-eudemo/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2026-present The Bluemira Team <>
+#
+# SPDX-License-Identifier: MIT
+
+
+"""Project overview."""
---
+++ bluemira-eudemo/plasma/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2026-present The Bluemira Team <>
+#
+# SPDX-License-Identifier: MIT
+"""Plasma."""
